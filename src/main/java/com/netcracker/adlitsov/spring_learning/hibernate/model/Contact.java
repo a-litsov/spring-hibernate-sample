120c74a5b3f1285211caf89504e6fde49dc81e2e
alex <adlitsov@gmail.com>
2018-04-23 07:37:34 +0300

    Contact.java: added Contact POJO class

---
+++ src/main/java/com/netcracker/adlitsov/spring_learning/hibernate/model/Contact.java
@@ -0,0 +1,70 @@
+package com.netcracker.adlitsov.spring_learning.hibernate.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="contact")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_date", nullable = false)
+    private Date birthDate;
+
+
+    public Contact() {
+    }
+
+    public Contact(String firstName, String lastName, Date birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
